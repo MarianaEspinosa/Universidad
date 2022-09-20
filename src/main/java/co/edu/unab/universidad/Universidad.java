04ca4a788f97c8e2a75d3c31844c3b2f3f029412
MarianaEspinosa <espinosam207@gmail.com>
2022-09-20 17:05:19 -0500

    Mensaje Actualizado

--- src/main/java/co/edu/unab/universidad/Universidad.java
+++ src/main/java/co/edu/unab/universidad/Universidad.java
@@ -1,17 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
- */
+
 
 package co.edu.unab.universidad;
 
-/**
- *
- * @author espin
- */
 public class Universidad {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Nuevo mensaje");
     }
 }
